2bd4307ccb1fe6b62e994517a69e790f6a14b401
Stefan O'Rear <stefanor@cox.net>
2015-12-12 03:08:19 -0800

    wip(2) now with some approximation of a design for the log cleaner

--- src/persist.rs
+++ src/persist.rs
@@ -1,69 +1,190 @@
-use std::fs::{self,File,OpenOptions};
-use std::path::{Path};
-use std::io::{self,ErrorKind};
+use std::fs::{self, File, OpenOptions};
+use std::path::{Path, PathBuf};
+use std::io::{self, ErrorKind, Read};
 use std::result;
 use std::str::FromStr;
-use std::collections::{HashSet};
+use std::collections::HashSet;
 use fs2::FileExt;
+use std::arc::Arc;
 
-struct Persister;
+pub struct Persister;
 
-enum Error {
-    BaseIsNotDir,
-    BaseStatFailed(io::Error),
-    LockOpenFailed(io::Error),
-    LockFailed(io::Error),
-    JournalOpendirFailed(io::Error),
-    JournalReaddirFailed(io::Error),
-    DeleteBadJournalFailed(io::Error),
-    JournalOpenFailed(io::Error),
+pub enum Error {
+    BaseIsNotDir(PathBuf),
+    BaseStatFailed(PathBuf, io::Error),
+    LockOpenFailed(PathBuf, io::Error),
+    LockFailed(PathBuf, io::Error),
+    JournalOpendirFailed(PathBuf, io::Error),
+    JournalReaddirFailed(PathBuf, io::Error),
+    DeleteBadJournalFailed(PathBuf, io::Error),
+    JournalOpenFailed(PathBuf, io::Error),
+    JournalReadFailed(PathBuf, io::Error),
 }
-type Result<T> = result::Result<T, Error>;
+
+pub type Result<T> = result::Result<T, Error>;
+
+pub enum ValueHandle {
+    SmallData {
+        data: (),
+    },
+    LargeData {
+        pointer: (),
+    },
+}
+
+impl ValueHandle {
+    pub fn get_type(&self) -> u16 {
+        unimplemented!()
+    }
+
+    pub fn read_copy(&self) -> Result<Vec<u8>> {
+        unimplemented!()
+    }
+}
+
+pub struct ValueIterator<'a> {
+    guts: &'a (),
+}
+impl<'a> Iterator for ValueIterator<'a> {
+    type Item = ValueHandle;
+    fn next(&mut self) -> Option<ValueHandle> {
+        unimplemented!()
+    }
+}
+
+pub struct Transaction {
+    guts: (),
+}
+impl Transaction {
+    pub fn add_item(&mut self, type_code: u16, data: Vec<u8>) -> Result<u64> {
+        unimplemented!()
+    }
+
+    pub fn del_item(&mut self, id: u64) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub fn get_item(&mut self, id: u64) -> Result<ValueHandle> {
+        unimplemented!()
+    }
+
+    pub fn list_items<'a>(&'a mut self, type_code: u16) -> Result<ValueIterator<'a>> {
+        unimplemented!()
+    }
+
+    pub fn commit(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+enum ObjectStorage {
+    Memory {
+        buf: Arc<Vec<u8>>,
+    },
+    File {
+        id: u64,
+        size: u64,
+        hash: u64,
+    },
+}
+
+struct ObjectInfo {
+    type_code: u16,
+    journal_id: u64,
+    storage: ObjectStorage,
+}
+
+struct TombstoneInfo {
+    journal_id: u64,
+}
+
+struct SegmentInfo {
+    live_object_ids: HashSet<u64>,
+    dead_object_ids: HashSet<u64>,
+    tombstone_ids: HashSet<u64>,
+    on_disk_size: u64,
+}
+
+struct JournalInfo {
+    segments: HashMap<u64, SegmentInfo>,
+    objects: HashMap<u64, ObjectInfo>,
+    tombstones: HashMap<u64, TombstoneInfo>,
+}
 
 impl Persister {
-    fn open(root: &Path, readonly: bool) -> Result<Self> {
+    pub fn transaction(&self) -> Transaction {
+        unimplemented!()
+    }
+
+    pub fn open(root: &Path, readonly: bool) -> Result<Self> {
         match fs::metadata(root) {
             Ok(meta) => {
                 if !meta.is_dir() {
-                    return Err(Error::BaseIsNotDir);
+                    return Err(Error::BaseIsNotDir(root.to_owned()));
                 }
-            },
+            }
             Err(err) => {
                 if err.kind() == ErrorKind::NotFound && !readonly {
                     unimplemented!();
                 } else {
-                    return Err(Error::BaseStatFailed(err));
+                    return Err(Error::BaseStatFailed(root.to_owned(), err));
                 }
-            },
+            }
         };
 
-        let locking_handle = try!(OpenOptions::new().write(true).open(root.join("yrola.lock")).map_err(Error::LockOpenFailed));
-        try!(locking_handle.lock_exclusive().map_err(Error::LockFailed));
+        let locking_path = root.join("yrola.lock");
+        let locking_handle = try!(OpenOptions::new()
+                                      .write(true)
+                                      .open(&locking_path)
+                                      .map_err(|e| Error::LockOpenFailed(locking_path.clone(), e)));
+        try!(locking_handle.lock_exclusive()
+                           .map_err(|e| Error::LockFailed(locking_path.clone(), e)));
 
         // read the logs
         let mut bad_journal_names = HashSet::new();
         let mut journal_names = Vec::new();
 
-        for rentry in try!(fs::read_dir(root.join("wals")).map_err(Error::JournalOpendirFailed)) {
-            let entry = try!(rentry.map_err(Error::JournalReaddirFailed));
-            match entry.file_name().to_str().and_then(|s|
-                u64::from_str(s).ok().and_then(|i| if format!("{:06}",i) == s { Some(i) } else { None })) {
-                    Some(jnum) => { journal_names.push(jnum); },
-                    None => { bad_journal_names.insert(entry.path()); },
-                };
+        let wals_path = root.join("wals");
+        let wals_iter = try!(fs::read_dir(&wals_path)
+                                 .map_err(|e| Error::JournalOpendirFailed(wals_path.clone(), e)));
+        for rentry in wals_iter {
+            let entry = try!(rentry.map_err(|e| Error::JournalReaddirFailed(wals_path.clone(), e)));
+            match entry.file_name().to_str().and_then(|s| {
+                u64::from_str(s).ok().and_then(|i| {
+                    if format!("{:06}", i) == s {
+                        Some(i)
+                    } else {
+                        None
+                    }
+                })
+            }) {
+                Some(jnum) => {
+                    journal_names.push(jnum);
+                }
+                None => {
+                    bad_journal_names.insert(entry.path());
+                }
+            };
         }
 
         journal_names.sort();
         for jnum in &journal_names {
-            let mut jfile = try!(File::open(root.join("wals").join(format!("{:06}", jnum))).map_err(Error::JournalOpenFailed));
+            let jpath = root.join("wals").join(format!("{:06}", jnum));
+            let mut jfile = try!(File::open(&jpath)
+                                     .map_err(|e| Error::JournalOpenFailed(jpath.clone(), e)));
+            let mut jdata = Vec::new();
+            try!(jfile.read_to_end(&mut jdata)
+                      .map_err(|e| Error::JournalReadFailed(jpath.clone(), e)));
             // TODO(now): Read the journal
+            unimplemented!();
         }
 
         unimplemented!();
 
         if !readonly {
             for badpath in bad_journal_names {
-                try!(fs::remove_file(badpath).map_err(Error::DeleteBadJournalFailed));
+                try!(fs::remove_file(&badpath)
+                         .map_err(|e| Error::DeleteBadJournalFailed(badpath.clone(), e)));
             }
         }
 
--- src/yrola.capnp
+++ src/yrola.capnp
@@ -3,21 +3,35 @@
 # MVP repository consists of a lock file, logs, and blobs
 # blobs are unstructured
 # log consists of a sequence of checksummed log records, followed by an incomplete record / zeros / end of file
-# log record = length (u64) checksum (u64) data(...)
+# log record = length (u32) length checksum (u32) checksum (u64) data(...)
 
-struct LogRecord {
-    type @0 :UInt32;
-    id @1 :UInt64;
+struct LogBlock {
     union {
-        data @2 :Data;
-        indirect @3 :IndirectRecord;
-        deleted @4 :Void;
+        segmentHeader :group {
+            previousSegmentIds @0 :List(UInt64);
+            highestEverItemId @6 :UInt64;
+        }
+        commit :group {
+            newInline @1 :List(InlineItem);
+            newExternal @2 :List(ExternalItem);
+            deleted @3 :List(UInt64);
+            obsoleteSegmentIds @4 :List(UInt64);
+        }
+        eof @5 :Void
     }
 }
 
-struct IndirectRecord {
-    size @0 :UInt64;
-    hash @1 :UInt64;
+struct InlineItem {
+    id @0 :UInt64;
+    type @1 :UInt16;
+    data @2 :Data;
+}
+
+struct ExternalItem {
+    id @0 :UInt64;
+    type @1 :UInt16;
+    size @2 :UInt64;
+    hash @3 : UInt64;
 }
 
 # Table manager
@@ -29,17 +43,17 @@ struct ColumnRun {
 
 struct TableRun {
     tableSchema @0 :UInt64;
-    firstPkey @0 :Data;
+    firstPkey @1 :Data;
     #tableInstance @0 :UInt64; # 0 = multi-instance, instance ID added after pkey
 
-    hasTimestamps @0 :Bool; # adds timestamp after PK
-    pkeyCount @0 :UInt32;
-    columnCount @0 :UInt32;
+    hasTimestamps @2 :Bool; # adds timestamp after PK
+    pkeyCount @3 :UInt32;
+    columnCount @4 :UInt32;
 
-    upserts @0 :List(ColumnRun);
-    deletes @0 :List(ColumnRun);
-    touches @0 :List(ColumnRun);
-    watches @0 :List(ColumnRun);
+    upserts @5 :List(ColumnRun);
+    deletes @6 :List(ColumnRun);
+    touches @7 :List(ColumnRun);
+    watches @8 :List(ColumnRun);
 }
 
 struct Run {
